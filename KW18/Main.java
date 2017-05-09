92fd8aa3480799603594e9316e1f1772c71d6fc1
Konstantin <konstantin.ruppel@informatik.hs-fulda.de>
2017-05-09 23:20:18 +0200

    KW19;  KW18 Lösungen; #1 Danke Benedikt für den Hinweis;

---
+++ KW18/Main.java
@@ -0,0 +1,204 @@
+/** Klasse zum Debuggen von Uebungsaufgaben */
+public class Main {
+    /** 
+     * Programm unterstuezt keine Parameter 
+     * @param args Werden nicht benutzt
+     */
+    public static void main (String[] args) {
+        
+        // Testen der letzten Aufgabe...
+        sieb s = new sieb();
+        int[] arr = s.erzeugeKandidaten(10);
+        s.filtereKandidaten3(arr);
+        s.druckeKandidaten(arr);
+    }
+
+    /** 
+     * Erste Aufgabe
+     */
+    int methode1 (int z) {
+        // if (z!=3)
+        //     return 0;
+        // else return 1;
+
+        return z != 3 ? 0 : 1;
+    }
+
+    /**
+     * Zweite Aufgabe
+     */
+    String methode2 (String x) {
+        // if (x.equals("hallo"))
+        //     return "hallo!";
+        // else 
+        //     return "tschuess";
+        
+        return x.equals("hallo") ? "hallo" : "tschuess";
+    }
+
+    /**
+     * Dritte Aufgabe mit Floats
+     */
+    float komplizierteBedingung (float i) {
+        // if(i<0.0f)
+        //     return -1f ;
+        // else if (i<3f)
+        //     return i ;
+        // else return 1f ;
+
+        return i < 0.0f ? - 1f : i < 3f ? i : 1f;
+    }
+
+    /**
+     * Vierte Aufgabe
+     */
+    int correctThis (int k) {
+    //void correctThis(int k) {
+        // if (k>0) {
+        //     int sum=0;
+        //     for (int i=0; i<k; i++)
+        //     sum += i;
+        // } else
+        //     sum=0 ;
+        // return sum ;
+
+        int sum=0;
+        if (k > 0) {
+            for (int i = 0; i<k; i++) {
+                sum += i;
+            }
+        } else {
+            sum=0 ;
+        }
+        return sum ;
+    }
+
+    /**
+     * Fuenfte Aufgabe
+     */
+    float correctThis2 (float x)
+    {
+        // float resultat = 1.0;
+        if (x >= 0.0)
+            // return resultat;
+            return 1.0f;
+        else
+            return 0.0f ;
+    }
+
+    /**
+     * Sechste Aufgabe: Druckt alle Elemente eines
+     * Arrays auf der Konsole aus. Ausgabe wird beendet
+     * wenn "end" ausgegeben wird.
+     * @param strings Auszugebende Strings
+     */
+    void druckeStrings1 (String[] strings) {
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println(strings[i]);
+            if (strings[i].equals("end")) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * Siebte Aufgabe: Liest Zeilen von der Konsole und
+     * gibt diese aus, solange bis "end" eingegeben wurde
+     */
+    void druckeStrings2 () {
+        // ohne import java.util.Scanner;
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String einString = "";
+        do {
+            einString = scanner.nextLine();
+            System.out.println(einString);
+        } while (!einString.equals("end"));
+    }
+}
+
+/**
+ * Stellt Funktionen bereit um Primzahlen herauszufiltern
+ */
+class sieb {
+
+    /**
+     * Liefert ein Array gefuellt mit [0 - limit-1]
+     */
+    public int [] erzeugeKandidaten (int limit) {
+        int[] kandidaten = new int[limit];
+        for (int i = 0; i < limit; i++)
+            kandidaten[i] = i;
+        return kandidaten;
+    }
+
+    /**
+     * Sieb des Eratosthenes:
+     * Filtert das uebergebene Array in dem alle 
+     * Nicht-Primzahlen durch -1 ersetzt werden
+     */
+    public void filtereKandidaten (int[] kandidaten) {
+        for (int p = 2; p < kandidaten.length; p++) {
+            for (int f = 2; p * f < kandidaten.length; f++) {
+                kandidaten[p * f] = - 1;
+            }
+        }
+        if (kandidaten.length >= 2) {
+            kandidaten[0] = -1;
+            kandidaten[1] = -1;
+        }
+    }
+    
+    /**
+     * Eine allgemeine Loesung zu filtereKandidaten
+     */
+    public void filtereKandidaten2 (int[] kandidaten) 
+    {
+        for(int i = 0; i < kandidaten.length; i++) {
+            if(kandidaten[i] < 2) {
+                kandidaten[i] = -1;
+            } else {
+                for(int j = 2; j < kandidaten[i] / 2 + 1; j++) {
+                    if(kandidaten[i] % j == 0)
+                        kandidaten[i] = -1;
+                }
+            }
+        }
+    }
+
+    /**
+     * Gibt alle Elemente des uebergebenen Arrays 
+     * untereinander auf der Konsole aus
+     */
+    void druckeKandidaten(int[] kandidaten) {
+      for(int i = 0; i < kandidaten.length; i++)
+        if(kandidaten[i] != -1) System.out.println(kandidaten[i]);
+    }
+    
+    /**
+     *  Komplett anderer Ansatz - Primzahlentest
+     */
+    public void filtereKandidaten3 (int[] kandidaten) {
+        for(int i = 0; i < kandidaten.length; i++) {
+            if(!test(kandidaten[i]))
+                kandidaten[i] = -1;
+        }
+    }
+
+    /**
+     * Sagt aus ob die uebergebene Zahl eine Primzahl ist
+     */
+    public static boolean test(long n) {
+        if (n <= 16)
+            return (n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || n == 13);
+        if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+            return false;
+        long b = (long) Math.sqrt (n) + 1;
+        for (long i = 10; i < b; i += 10) {
+            if ((n % (i + 1) == 0) 
+                || (n % (i + 3) == 0) 
+                || (n % (i + 7) == 0) 
+                || (n % (i + 9) == 0)) return false;
+        }
+        return true;
+    }
+}
--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # Übungen zur Vorlesung "Programmierung 2" ![](https://www.hs-fulda.de//typo3conf/ext/hfd/Resources/Public/Images/Fulda_footer_logo.svg)
 
 ## Neu
-* Einige [Anmerkungen](/KW18/170504.md) zu JavaDoc hinzugefügt
-* Einfachere [Methode](/KW17/Main.java) für die hex-int-Umrechnung hinzugefügt
-* Übungen lokal debuggen - [Beispiel](/KW18/170504.md) erstellt
+* [Übersicht](/KW19/170511.md) primitiver Datentypen
+* Lösungen für die KW18 hochgeladen
 
 ## Wochenübersicht
 | Übung                         | Stichpunkte
 |-                              |-
-| [KW17 [27.04.2017]](/KW17)    | Ternäre Operator ● Do-While-Schleife ● break ● continue ● Typumwandlungen ● Inkrement-/Dekrement-Operatoren ● Präzedenz von Operatoren ● Sichtbarkeit von Variablen ● [Erfrischung Prog1](/KW17/Wiederholung.java) ● [Lösungen](/KW17/Main.java) ● [Zusammenfassung](/KW17/170427.md)
-| [KW18 [04.05.2017]](/KW18)    | Primzahlen ● Sieb des Eratosthenes ● Guter/schlechter Code ● JavaDoc ● Coding Conventions ● Google Java Style ● Style Checker ● [Zusammenfassung](/KW18/170504.md)         
-| KW19 [11.05.2017]             |
+| [KW17 [27.04.2017]](/KW17)    | Ternäre Operator ● Do-While-Schleife ● break ● continue ● Typumwandlungen ● Inkrement-/Dekrement-Operatoren ● Präzedenz von Operatoren ● Sichtbarkeit von Variablen ● [Erfrischung Prog1](/KW17/Wiederholung.java) ● [Lösungen](/KW17/Main.java) ● [Weiteres](/KW17/170427.md)
+| [KW18 [04.05.2017]](/KW18)    | Primzahlen ● Sieb des Eratosthenes ● Guter/schlechter Code ● JavaDoc ● Coding Conventions ● Google Java Style ● Style Checker ● [Lösungen](/KW18/Main.java) ● [Weiteres](/KW18/170504.md)         
+| [KW19 [11.05.2017]](/KW19)    | Compiler ● Java Virtual Machine ● Portabilität ● Machinencode ● Speicherverwaltung ● Referenzvariable ● new ● Primitive Datentypen ● Objekte ● Garbage Collection ● [Weiteres](/KW19/170511.md)
 | KW20 [18.05.2017]             |
 | KW21 [25.05.2017]             | Fällt aus - *Christi Himmelfahrt*
 | KW22 [01.05.2017]             |
